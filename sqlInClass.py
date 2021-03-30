077a2c689881a027c54d04c9934755f314d3aa36
ottovenliu <76770643+ottovenliu@users.noreply.github.com>
2021-03-30 13:58:13 +0800

    basic_0330_1

--- sqlInClass.py
+++ sqlInClass.py
@@ -7,3 +7,37 @@ mydb = mysql.connector.Connect(
     database="website"
 )
 # print(mydb)
+text = "小蓮花"
+mycursor = mydb.cursor()
+#sql = "SELECT * FROM user WHERE name='{}'" .format(text)
+sql = "SELECT * FROM user"
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+print(myresult[0][2])
+print(myresult[0])
+print(myresult)
+print("------------------分界線1--------------------")
+print(type(myresult))
+
+# if myresult[1][2] == text:
+#     print(myresult[1][1])
+userdatainfo = list(myresult[0])
+print(userdatainfo)
+print("------------------分界線2--------------------")
+userDataTable = []
+userDataBox = []
+for i in myresult:
+    for j in myresult:
+        userDataBox.append(j)
+    userDataTable.append(userDataBox)
+
+print(userDataBox)
+
+# for i in myresult:
+#     a = myresult[i]
+#     list(a)
+#     # userdatainfo = list(myresult[0])
+#     userDataBox.append(a)
+#     print(userDataBox)
+# if text == myresult[i][2]:
+#     print(myresult[i][1])
